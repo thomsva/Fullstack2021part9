1fdf2dcfd52018823cb0982112f9985453b79e57
thomsva <thomsva@gmail.com>
2022-03-25 14:46:12 +0200

    9.9 ok

--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+const PORT = 3001;
 
-const PORT = 3000;
-
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
